925e4b698beb797326e295b0b85bcdd5d16e5ed2
Dilshad <dhaji2@huskers.unl.edu>
2020-08-21 19:11:04 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+ /**
+ * Author: Dilshad Haji
+ * Date: 2020
+ *
+ * A simple hello world program
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+
+int main(int argc, char **argv) { 
+    
+  printf("Dilshad Haji!\n"); 
+  printf("Computer Science & Cyber security\n");
+  
+ return 0; 
+ 
+}
+    
